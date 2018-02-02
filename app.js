7ccbc4df5c72e729dc293b074e049655930daee4
jamarboledas <jamarboledas@gmail.com>
2018-02-02 23:16:05 +0100

    Log module has been installed

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const router = require('./api/router');
 const app = express();
-
 const mongoose = require('mongoose');
-
-
+const Log = require('log');
+const log = new Log('info');
 
 const db = mongoose.createConnection('mongodb://localhost:27017/nodeJS');
-db.on('error', console.error.bind(console, 'mongoose connection error: '));
-db.once('open', () => {
-    console.log("Connected");
-});
-
+db.on('error', log.error.bind(console, 'mongoose connection error: '));
+db.once('open', () => log.info('Connected'));
 
 app.use(router);
 app.listen(3000, () => true);
