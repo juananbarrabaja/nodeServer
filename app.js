23f8bad0a6c382d1be6625af7d189588cd644996
jamarboledas <jamarboledas@gmail.com>
2018-02-02 23:04:45 +0100

    mongoose model has been installed and set up

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ const express = require('express');
 const router = require('./api/router');
 const app = express();
 
+const mongoose = require('mongoose');
+
+
+
+const db = mongoose.createConnection('mongodb://localhost:27017/nodeJS');
+db.on('error', console.error.bind(console, 'mongoose connection error: '));
+db.once('open', () => {
+    console.log("Connected");
+});
+
+
 app.use(router);
 app.listen(3000, () => true);
